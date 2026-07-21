ee62a89c48f26a06ff9e1eb44d13009e351e2e75
omercanb <omercanbaykara@gmail.com>
2026-07-21 09:20:07 +0300

    Delete files unused in refactor
    Deleted everything to do with program analysis

--- main.py
+++ main.py
@@ -9,67 +9,11 @@ from mypy.options import Options
 from mypy.traverser import TraverserVisitor
 from mypy.types import CallableType, Type
 
-from python.analysis.name_resolution import NameResolver
-from python.analysis.scope import ScopeTreeCreator
-from python.analysis.symbol_declaration import SymbolDefiner
-from python.analysis.type_annotation import (
-    ClassTypeDeclarer,
-    FunctionAndClassTypeAnnotator,
-    TypeAnnotator,
-)
-from python.analysis.type_inference import TypeInferrer
-from python.codegen.codegen import CppTranslator
 from python.codegen.mypy_codegen import StatementCodegen
 from python.formatting import *
-from python.utils import build_and_run, dump
+from python.utils import build_and_run
 
 includes = ["print.h", "list.h", "ptr.h"]
-
-
-def pipeline(program: str, debug=False):
-    tree = ast.parse(program)
-    if debug:
-        print(dump(tree, indent=4))
-    # return
-    # validate.Validator().visit(tree)
-
-    scope_tree_creator = ScopeTreeCreator()
-    scope_tree_creator.visit(tree)
-    node_scopes = scope_tree_creator.node_scopes
-
-    symbol_definer = SymbolDefiner(node_scopes)
-    symbol_definer.visit(tree)
-
-    name_resolver = NameResolver(node_scopes)
-    name_resolver.visit(tree)
-    bindings = name_resolver.bindings
-    if debug:
-        print_bindings(bindings)
-
-    class_declarer = ClassTypeDeclarer(node_scopes, bindings)
-    class_declarer.visit(tree)
-    types = class_declarer.types
-
-    type_annotator = FunctionAndClassTypeAnnotator(node_scopes, bindings, types)
-    type_annotator.visit(tree)
-
-    # type_annotator = TypeAnnotator(node_scopes, bindings, types)
-    # type_annotator.visit(tree)
-
-    try:
-        type_inference = TypeInferrer(node_scopes, bindings, types)
-        type_inference.visit(tree)
-    finally:
-        if debug:
-            print(typed_unparse(tree, types))
-    return
-
-    translator = CppTranslator(node_scopes, bindings, types)
-    translator.visit(tree)
-    s = translator.flush()
-    return s
-
-    # print(dump(tree, ptypes=ptypes, indent=4))
 
 
 def validate(path: str):
